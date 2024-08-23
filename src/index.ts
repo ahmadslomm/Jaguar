e6e0bd94e49439c9a6f418b52dbae90d4aa8ae03
Devang Patel <devangpatel@devang2210.local>
2024-08-23 21:45:51 +0530

    changes in the boat config

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ if(token) {
 
         console.log("Getting the redirect URL......", redirectURL)
      
-        checkuserRegistrationStatus && (ctx.replyWithMarkdownV2(`*Hello! Welcome to the telegram bot*`,{
+        checkuserRegistrationStatus && (ctx.replyWithMarkdownV2(`*Hello\\! Welcome to the telegram bot*`,{
             reply_markup: {
                 inline_keyboard: [
                     [{text: 'Click me', web_app: { url : redirectURL}}]
